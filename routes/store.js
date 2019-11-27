0f2e7a4b0d9c32145130a99fa605f75354c3c768
wynncreations <wynncreations@gmail.com>
2019-11-26 22:28:41 -0600

    email is working.

--- routes/store.js
+++ routes/store.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const StoreEvent = require("../models/store_event");
 const Item = require('../models/item');
+const Kid = require('../models/kids');
 const User = require('../models/users');
 const sgMail = require('@sendgrid/mail');
- sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+ sgMail.setApiKey('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');
 
 
 router.post('/purchase',(req,res,next)=>{
@@ -21,43 +23,46 @@ router.post('/purchase',(req,res,next)=>{
             res.status(501).send(`Error saving event - ${err}`);
         }
     });
-    
 
+    var scout = '';
+
+    Kid.findOne({_id:req.body.scout_id},(err,kid)=>{
+        if (err) {
+            res.status('502').send(err)
+        }
+
+        scout = kid
+    });
+    
     //var admins;
-    Item.findById({_id:req.body.item_id},(err,doc)=>{
-        var item = JSON.stringify(doc)
-    
-    
+    Item.findOne({_id:req.body.item_id},(err,doc)=>{
+        if(err){res.status('502').send(err)}
+        
             const msg = {
                 to: req.body.parent_email,
-                //cc: 'robert+scoutsgearedadmin@gmail.com', //Admin account
+                cc: 'robert+scoutsgearedadmin@wynnoutfitters.com', //Admin account
                 from: 'robert@wynnoutfitters.com',
-                subject: `Purchase of ${item.name} Confirmation`,
-                text: `Congratulations, you have reserved  ${item.name} for the cost of $${req.body.retail_cost}. A follow up email will be sent once the item is available for pickup at your next meeting.`,
-                html: `<strong>Congratulations, you have reserved  ${item.name} for the cost of $${req.body.retail_cost}. A follow up email will be sent once the item is available for pickup at your next meeting.</strong>`
+                subject: `Purchase of ${doc.name} Confirmation`,
+                text: `Congratulations, ${scout.firstname} ${scout.lastname} has reserved  ${doc.name} for the cost of $${req.body.retail_cost}. A follow up email will be sent once the item is available for pickup at your next meeting.`,
+                html: `<strong>Congratulations, ${scout.firstname} ${scout.lastname} has reserved  ${doc.name} for the cost of $${req.body.retail_cost}. A follow up email will be sent once the item is available for pickup at your next meeting.</strong>`
             };
-        console.log(msg)
         const status = sgMail.send(msg);
-        res.status(200).send({
-            message: `${status}`
-        });
+        
+            console.log((parseFloat(req.body.retail_cost) * -1))
+            //update scout fund total
+            Kid.findOneAndUpdate({
+                _id: req.body.scout_id
+            }, {
+                $inc: {
+                    scoutFund: (parseFloat(req.body.retail_cost) * parseFloat(-1))
+                }
+            }, (err, doc) => {
+                res.status(200).send({
+                    message: `Successfully logged event`
+                });
+            });
     
     });
-
-    /*User.find({account_type:"Admin"},(err,doc)=>{
-        admins = doc.json();
-    });*/
-
-    //console.log(item);
-        
-
-/*
-    //update scout fund total
-    User.findByIdAndUpdate({_id:req.body.scout_id},{$inc:{total_raised:(parseFloat(req.body.retail_cost)*-1)}},(err,doc)=>{
-        res.status(200).send({message:`Successfully logged event ${req.body}`});
-    });
-    */
-
 
 });
 
